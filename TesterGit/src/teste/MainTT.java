3e1dca99ff54841bb0b0244aedd8a5f0131a2e18
Ui <Ui@192.168.1.6>
2016-09-19 23:36:55 +0100

    commit comment

--- TesterGit/src/teste/MainTT.java
+++ TesterGit/src/teste/MainTT.java
@@ -3,8 +3,33 @@ package teste;
 public class MainTT {
 
 	public static void main(String[] args) {
+
+		// @WebMethod
+		// public boolean versement(@WebParam(name="codeCompte") String
+		// codeCompte,
+		// @WebParam(name="montant") double montant,
+		// @WebParam(name="codeEmplye") long codeEmplye) {
+		// return operationMetier.versement(codeCompte, montant, codeEmplye);
+		// }
+		// @WebMethod
+		// public boolean retrait(@WebParam(name="codeCompte") String
+		// codeCompte,
+		// @WebParam(name="montant") double montant,
+		// @WebParam(name="codeEmplye") long codeEmplye) {
+		// return operationMetier.retrait(codeCompte, montant, codeEmplye);
+		// }
+		// @WebMethod
+		// public boolean virement(@WebParam(name="codeCompte1") String
+		// codeCompte1,
+		// @WebParam(name="codeCompte2") String codeCompte2,
+		// @WebParam(name="montant") double montant,
+		// @WebParam(name="codeEmplye") long codeEmplye) {
+		// return operationMetier.virement(codeCompte1, codeCompte2, montant,
+		// codeEmplye);
+		// }
+		//
 		System.out.println("hello repository !");
-		System.out.println("hello repository after !");
+		//
 
 	}
 
